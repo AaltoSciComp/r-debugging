b02373050aad88719c0cc45fd03b338f9bfbec04
Simo Tuomisto <simo.tuomisto@aalto.fi>
2022-04-25 10:14:28 +0300

    chapter 3: First version of chapter 3

---
+++ examples/missing-variables.R
@@ -0,0 +1,16 @@
+# Ensure that z is missing
+if (exists("z")) {
+  rm(z)
+}
+g <- function(x) {
+  return(x + z)
+}
+
+f <- function(x,y = g(x)) {
+  if (x>0) {
+    return(x)
+  } else {
+    return(g(x))
+  }
+}
+f(-1)
